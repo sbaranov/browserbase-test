125010a46d224bc83c6a09f9443f86186db4e291
Stas Baranov <stanislav@gmail.com>
2025-02-26 12:25:43 -0800

    clean up

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import os
-from dotenv import load_dotenv
+
 from browserbase import Browserbase
+from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
 
+load_dotenv()
+
+BROWSERBASE_API_KEY = os.environ["BROWSERBASE_API_KEY"]
+BROWSERBASE_PROJECT_ID = os.environ["BROWSERBASE_PROJECT_ID"]
+
+bb = Browserbase(api_key=BROWSERBASE_API_KEY)
+
 def main():
-    load_dotenv()
-    bb = Browserbase()
-    session = bb.sessions.create(project_id=os.getenv('BROWSERBASE_PROJECT_ID'))
+    session = bb.sessions.create(project_id=BROWSERBASE_PROJECT_ID)
+    print("Session replay URL:", f"https://browserbase.com/sessions/{session.id}")
 
     with sync_playwright() as p:
         browser = p.chromium.connect_over_cdp(session.connect_url)
-        default_context = browser.contexts[0]
+        context = browser.contexts[0]
+        page = context.pages[0]
 
-        page = default_context.pages[0]
         page.goto("https://example.com")
-        page.get_by_role("button", name="Submit").click()
+        # page.get_by_role("button", name="Submit").click()
         page.screenshot(path="screenshot.png")
-        page.close()
 
+        page.close()
         browser.close()
-        print(f"Session complete! View replay at https://browserbase.com/sessions/{session.id}")
+        print("Done!")
 
 if __name__ == "__main__":
     main()
