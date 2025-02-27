5ddce0d4ea2201ce9e9cb3d67726528bd821ab2e
Stas Baranov <stanislav@gmail.com>
2025-02-26 17:54:07 -0800

    goto products

--- main.py
+++ main.py
@@ -13,6 +13,22 @@ BROWSERBASE_PROJECT_ID = os.environ["BROWSERBASE_PROJECT_ID"]
 
 bb = Browserbase(api_key=BROWSERBASE_API_KEY)
 
+def get_product_asins(page, query):
+    page.goto("https://amazon.com")
+    page.get_by_placeholder("Search Amazon").fill(query)
+    page.locator('#nav-search-submit-button').click()
+    page.wait_for_timeout(1000)
+
+    product_asins = []
+    for result in page.locator('.s-result-item').all():
+        data_asin = result.get_attribute('data-asin')
+        if not data_asin:
+            continue
+
+        product_asins.append(data_asin)
+
+    return product_asins
+
 def main():
     session = bb.sessions.create(project_id=BROWSERBASE_PROJECT_ID)
     print("Session replay URL:", f"https://browserbase.com/sessions/{session.id}")
@@ -22,14 +38,15 @@ def main():
         context = browser.contexts[0]
         page = context.pages[0]
 
-        page.goto("https://amazon.com")
-        page.get_by_placeholder("Search Amazon").fill(AMAZON_SEARCH_QUERY)
-        page.locator('#nav-search-submit-button').click()
-        page.screenshot(path="screenshot.png")
+        product_asins = get_product_asins(page, AMAZON_SEARCH_QUERY)
+        for product_asin in product_asins[:3]:
+            print(f"https://amazon.com/dp/{product_asin}")
+            page.goto(f"https://amazon.com/dp/{product_asin}")
+            page.wait_for_timeout(1000)
 
         page.close()
         browser.close()
-        print("Done!")
+        print("Done")
 
 if __name__ == "__main__":
     main()
